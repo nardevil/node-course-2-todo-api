199fe55ba62dad0690f189d2d0e21ee7a5f470b4
vprica3@gmail.com <vprica3@gmail.com>
2018-08-14 22:44:03 +0200

    AddUpdateScript

---
+++ playground/mongodb-update.js
@@ -0,0 +1,41 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, client) => {
+  if (error){
+    return console.log('Unable to connect to MongoDB server!!!');
+  }
+  console.log('Connected to MongoDB server');
+
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5b7339825189c9b40b5bb295')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5b72e1854d6e5f16fcb4d92b')
+  }, {
+    $set: {
+      name: 'Ronaldo'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+   // client.close();
+});
